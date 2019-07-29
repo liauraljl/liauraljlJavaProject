c35f8c7c9320f272fab3d4b42d77d18d5b828033
jinlei.li_ <jinlei.li@weimob.com>
2019-07-29 22:58:59 +0800

    add Singleton 7种方式实现

---
+++ ApplicationProjectTest/src/main/java/test1/SingletonTest/Singleton1.java
@@ -0,0 +1,17 @@
+package test1.SingletonTest;
+
+/**
+ * 单例 懒汉模式
+ * Created by liaura_ljl on 2019/7/29.
+ */
+public class Singleton1 {
+    private static Singleton1 instance=null;
+
+    private Singleton1(){}
+
+    public static Singleton1 getInstance(){
+        if(null==instance)
+            instance=new Singleton1();
+        return instance;
+    }
+}
