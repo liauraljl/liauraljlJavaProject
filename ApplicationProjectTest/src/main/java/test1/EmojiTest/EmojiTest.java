cf786c81578d513112844d263823cc2905dffeae
jinlei.li_ <jinlei.li@weimob.com>
2019-09-19 08:46:39 +0800

    add emoji

---
+++ ApplicationProjectTest/src/main/java/test1/EmojiTest/EmojiTest.java
@@ -0,0 +1,18 @@
+package test1.EmojiTest;
+
+import com.vdurmont.emoji.EmojiParser;
+
+/**
+ * Created by liaura_ljl on 2019/9/19.
+ */
+public class EmojiTest {
+    public static void main(String[] args){
+        String str = "An :grinning:awesome :smiley:string 😄with a few :wink:emojis :ac: :tv: :writing: :writing_hand: !";
+        String result = EmojiParser.parseToUnicode(str);
+        System.out.println(result);
+
+        result = EmojiParser.parseToAliases(str);
+        System.out.println(result);
+
+    }
+}
