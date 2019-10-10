d41223078c8f4ae2db5c2a36f891c007a2cb8952
jinlei.li <jinlei.li@weimob.com>
2019-10-10 19:06:51 +0800

    默认JDK版本修改

--- demo1/src/main/java/com/ljl/example/Demo1Application.java
+++ demo1/src/main/java/com/ljl/example/Demo1Application.java
@@ -1,17 +1,21 @@
 package com.ljl.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
-public class Demo1Application {
+public class Demo1Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Demo1Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
