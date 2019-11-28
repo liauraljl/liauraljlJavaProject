3343ea63785e3f461f0d1a63896ce34d9f47e268
jinlei.li <liaura_ljl@163.com>
2019-11-28 17:25:26 +0800

    feat:test

--- ApplicationProjectTest/src/main/java/test1/stringTest/StringArrSplitTest.java
+++ ApplicationProjectTest/src/main/java/test1/stringTest/StringArrSplitTest.java
@@ -1,6 +1,8 @@
 package test1.stringTest;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by liaura_ljl on 2018/5/22.
@@ -18,5 +20,27 @@ public class StringArrSplitTest {
                 System.out.print("c");
             }
         }
+
+        String topics1="sds;dsfd";
+        String topics2="fdsfds";
+        String[] tp1=topics1.split(";");
+        String[] tp2=topics2.split(";");
+
+        Map<String, Map<Integer,Long>> mapT=new HashMap<>();
+        Map<Integer,Long> map=new HashMap<>();
+        mapT.put("m1",map);
+        Map<Integer,Long> map2=mapT.get("m1");
+        //map2.put(1,333L);
+        Map<Integer, Long> currentOffset = new HashMap();
+        currentOffset.put(2,44L);
+        map2=currentOffset;
+        try {
+            Thread.sleep(1000000000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println();
     }
 }
