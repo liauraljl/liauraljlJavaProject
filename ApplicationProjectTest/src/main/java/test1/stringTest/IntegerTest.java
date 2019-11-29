dadf82a84a801dac834a88f2188093464f5e3f45
jinlei.li_ <liaura_ljl@163.com>
2019-11-29 22:29:59 +0800

    feat:update xmind

--- ApplicationProjectTest/src/main/java/test1/stringTest/IntegerTest.java
+++ ApplicationProjectTest/src/main/java/test1/stringTest/IntegerTest.java
@@ -4,7 +4,11 @@ package test1.stringTest;
  * Created by liaura_ljl on 2018/7/24.
  */
 public class IntegerTest {
+
+    private static int a;
+
     public static void main(String[] args){
+        System.err.println(a);
         Integer a=12345678;
         Integer b=12345678;
         boolean c=(a==b);
