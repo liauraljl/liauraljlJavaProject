751383ef4854ac9179415a814d202f05213d58ea
jinlei.li_ <liaura_ljl@163.com>
2019-12-01 09:44:47 +0800

    feat:类加载 双亲委派 test

---
+++ ApplicationProjectTest/src/main/java/java/lang/String.java
@@ -0,0 +1,10 @@
+package java.lang;
+
+/**
+ * Created by liaura_ljl on 2019/12/1.
+ */
+public class String {
+    public String(String ss){
+        System.out.println("我自己的类："+ss);
+    }
+}
---
+++ ApplicationProjectTest/src/main/java/test1/stringTest/ClassLoaderTest.java
@@ -0,0 +1,12 @@
+package test1.stringTest;
+
+/**
+ * Created by liaura_ljl on 2019/12/1.
+ */
+public class ClassLoaderTest {
+    public static void main(String[] args) {
+        String dsdf="d";
+        String dfsdf=new String("11111111111fdsfdsfgds");
+        System.out.println("load end");
+    }
+}
