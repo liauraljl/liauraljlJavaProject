e5193f31adf04ffc67fae22a33e043527bc85dfa
jinlei.li_ <liaura_ljl@163.com>
2019-12-09 18:42:00 +0800

    feat:wait notify 交替奇偶数打印

---
+++ ApplicationProjectTest/src/main/java/test1/threadTest/ThreadTest4.java
@@ -0,0 +1,48 @@
+package test1.threadTest;
+
+/**
+ * Created by liaura_ljl on 2019/12/9.
+ */
+public class ThreadTest4 {
+    public static void main(String[] args) {
+        /*Thread t1 = new Thread();
+        t1.start();
+        //t1.start();//会检查并修改线程状态  只能调用一次
+        t1.interrupt();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        t1.isInterrupted();
+        try {
+            new ThreadTest4().wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        new ThreadTest4().notify();*/
+        ThreadTest4 t4 = new ThreadTest4();
+        new Thread(() -> {
+            try {
+                t4.test1a();
+            } catch (InterruptedException e) {
+            }
+        }).start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        synchronized (t4){
+            t4.notify();//wait notify notifyAll必须在同步代码块内执行
+        }
+    }
+
+    private void test1a() throws InterruptedException {
+        synchronized (this) {
+            System.out.println("wait");
+            this.wait();
+            System.out.println("notify");
+        }
+    }
+}
---
+++ ApplicationProjectTest/src/main/java/test1/threadTest/ThreadTest5_交替奇偶.java
@@ -0,0 +1,47 @@
+package test1.threadTest;
+
+/**
+ * Created by liaura_ljl on 2019/12/9.
+ * 交替打印奇偶数
+ */
+public class ThreadTest5_交替奇偶 {
+
+    private static final Object obj = new Object();
+
+    private static volatile int count = 0;
+
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            synchronized (obj) {
+                while (count < 100 && (count & 1) == 0) {//与1按位与
+                    System.out.println(count++ + "是偶数！");
+                    try {
+                        obj.notify();
+                        if(count<100){
+                            obj.wait();
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            synchronized (obj) {
+                while (count < 100 && count % 2 == 1) {
+                    System.out.println(count++ + "是奇数！");
+                    try {
+                        obj.notify();
+                        if(count<100){
+                            obj.wait();
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+}
