65f1ba854a5382f95937b80b3b94a45276bd442a
jinlei.li_ <liaura_ljl@163.com>
2020-04-09 09:16:01 +0800

    feat:ratelimiter并发测试

--- ApplicationProjectTest/src/main/java/test1/rateLimiterTest/RateLimiterTest.java
+++ ApplicationProjectTest/src/main/java/test1/rateLimiterTest/RateLimiterTest.java
@@ -4,30 +4,38 @@ import com.google.common.util.concurrent.RateLimiter;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class RateLimiterTest {
     public static void main(String[] args) {
-        ExecutorService exec= Executors.newCachedThreadPool();
-        final RateLimiter rateLimiter=RateLimiter.create(3.0);
+        ExecutorService executorService= Executors.newFixedThreadPool(10);
+        final RateLimiter rateLimiter=RateLimiter.create(9);
+        AtomicInteger count=new AtomicInteger();
+        CountDownLatch countDownLatch=new CountDownLatch(100);
         for(int i=0;i<100;i++){
-            final int no=i;
-            Runnable runnable=new Runnable() {
-                public void run() {
-                    try{
-                        //获取许可
-                        rateLimiter.acquire();
-                        System.out.println("Accessing: " + no + ",time:"
-                                + new SimpleDateFormat("yy-MM-dd HH:mm:ss").format(new Date()));
-                    }catch (Exception e){
-                        e.printStackTrace();
-                    }
+            executorService.submit(()->{
+                try{
+                    //获取许可
+                    rateLimiter.acquire();
+                    System.out.println("Accessing: " + count.incrementAndGet() + ",time:"
+                            + new SimpleDateFormat("yy-MM-dd HH:mm:ss").format(new Date()));
+                    countDownLatch.countDown();
+                    System.out.println(Thread.currentThread().getName());
+                }catch (Exception e){
+                    e.printStackTrace();
                 }
-            };
-            exec.execute(runnable);
+            });
+
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         //退出线程池
-        exec.shutdown();
+        executorService.shutdown();
     }
 }
